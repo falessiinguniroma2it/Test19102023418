030d46162c7012608f27d33084737a71e592a8a3
Davide <falessi@ing.uniroma2.it>
2023-10-19 16:32:49 +0200

    Esempio di secondo commit con due altri smell.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,8 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
+        int a;
+        //prassi demodè
 
         }
     }
